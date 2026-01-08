05556b5fd649cb87a183d66e7a3a7761a539d3e0
jonathanlukas19 <jml33298@creighton.edu>
2026-01-07 21:54:50 -0600

    Revise registration page content and links
    Updated text and removed Venmo link.

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -46,27 +46,14 @@ export default function Registration() {
         <main className="max-w-3xl mx-auto">
           <div className="text-gray-900 space-y-8 text-base md:text-lg leading-relaxed">
             <p>
-              Already have your class arranged and a start date has been established? Great! Please click{" "}
-              <Link
-                href="https://www.venmo.com/u/Fourwindslounge"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-emerald-700 hover:text-emerald-800 underline font-semibold"
-              >
-                here
-              </Link>
-              . We can't wait to work together!
-            </p>
-
-            <p>
-              If you haven't arranged a class date and time, but are interested learning Mah Jongg, please click{" "}
+              Please click{" "}
               <Link
                 href="mailto:amylukas@fourwindslounge.com"
                 className="text-emerald-700 hover:text-emerald-800 underline font-semibold"
               >
                 here
               </Link>{" "}
-              to message Amy Lukas or Kitty Gaenslen. We'd love to work with you!
+              to message our team.
             </p>
           </div>
         </main>
@@ -74,4 +61,3 @@ export default function Registration() {
     </div>
   )
 }
-
