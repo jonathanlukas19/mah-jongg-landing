bf63144f2d537a13e37a59b5928b94221475894c
jonathanlukas19 <jml33298@creighton.edu>
2026-01-07 22:23:19 -0600

    Update contact email for event inquiries

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -60,7 +60,7 @@ export default function CorporateClubCustomers() {
             <p>
               Ready to plan YOUR unique Mah Jongg event?{" "}
               <Link
-                href="mailto:amylukas@fourwindslounge.com"
+                href="mailto:info@fourwindslounge.com"
                 className="text-emerald-700 hover:text-emerald-800 underline font-semibold"
               >
                 Reach out today!
